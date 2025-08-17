2b0a939a73445aad5c221977ca33cb80cf434b96
Soham Haran <75292663+haransoham@users.noreply.github.com>
2025-08-17 10:22:17 +0000

    project 1

---
+++ calculate.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void main(){
+
+    while(1){
+        system("clear");
+        float number1 =0, number2=0,ans=0;
+        char operation,again;
+        printf("\n-----------------------------------------------\n");
+        printf("\n\tWelcome to the Calculator Program!\n");
+        printf("\n-----------------------------------------------\n\n");
+
+        printf("\tEnter first number:\t");
+        scanf("%f",&number1);
+        printf("\n\tEnter second number:\t");
+        scanf("%f",&number2);
+        printf("\n\tEnter operation (+, -, *, /):\t");
+        scanf("\n%c", &operation);
+        switch(operation){
+            case '+':
+                ans=number1+number2;
+                break;
+            case '-':
+                ans=number1-number2;
+                break;
+            case '*':
+                ans=number1*number2;
+                break;
+            case '/':
+                if(number2!=0){
+                    ans=number1/number2;    
+                        
+                }else{
+                    printf("Error: Division by zero is not allowed");
+                }
+                break;
+            default:
+             printf("Error: Wrong operation selection");
+        }
+        printf("\n-----------------------------------------------\n");
+        printf("  Answer:  %f %c %f = %f  \n",number1,operation,number2,ans);
+        printf("\n-----------------------------------------------\n\n");
+
+
+
+        printf("would you like to calculate again (y/n):");
+        scanf("\n%c",&again);
+        if(again!='y'){
+            break;
+        }
+
+    }
+
+}
